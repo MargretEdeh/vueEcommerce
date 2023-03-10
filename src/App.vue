313a64ddeabe4d99d2822ba07be8f50be81b2f51
Margret Edeh <sagaedeh@gmail.com>
2023-03-09 16:29:56 -0800

    added the vue router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div>
-    <div class="bg-white py-3 " >
-      <div>
-        <h1 class="text-primary text-3xl px-5">StarCart</h1>
-      </div>
-    </div>
-  <div class="lottie relative">
-<Vue3Lottie :animationData=" theAnimation "   :loop="true" :autoplay="true" :rendererSettings="{preserveAspectRatio: 'xMidYMid slice'}" />
-  </div>
-  <div> 
-  <p class="text-center">Made with ❤️ by </p> 
-</div>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-<script  setup>
-import {computed} from 'vue'
-import {Vue3Lottie} from 'vue3-lottie' 
-import animationData from  './animationData.json'
-import 'vue3-lottie/dist/style.css'
-const theAnimation =computed(() => animationData)
-</script>
-<style scoped>
-.lottie {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: -1;
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
